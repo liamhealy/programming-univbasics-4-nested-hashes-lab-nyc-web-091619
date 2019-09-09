c534e3a331bac02676031de66b00adb1bde03648
Liam Healy <lhealy0711@gmail.com>
2019-09-09 01:34:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:key] = "value"
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["Lisp", "C"]
+  }
     
 end
 
